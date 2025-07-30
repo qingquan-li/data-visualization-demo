2bbbda91171245fab635286106f21bc409e52eaa
Qingquan Li <liqqisme@gmail.com>
2025-07-30 09:38:19 -0400

    Update RootAPIURL

--- frontend/src/components/RootAPIURL.ts
+++ frontend/src/components/RootAPIURL.ts
@@ -1,2 +1,2 @@
-// export const RootAPIURL: string = "http://localhost:8080/api"; // For local development
-export const RootAPIURL: string = '/api'; // For production
+export const RootAPIURL: string = "http://localhost:8080/api"; // For local development
+// export const RootAPIURL: string = '/api'; // For production
